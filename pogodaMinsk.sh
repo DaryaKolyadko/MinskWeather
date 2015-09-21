0dd952b140ebb6b636f0a740841e4e0a9a651906
DaryaKolyadko <skvoznak1996@gmail.com>
2015-09-21 18:24:28 +0300

    config and script files

---
+++ pogodaMinsk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# shows forecast for Minsk each 5 seconds
+. ./config.ini
+URL=https://pogoda.yandex.by/
+while true
+do
+	wget $URL -q index.html
+	grep -E -o '<div class="current-weather__thermometer current-weather__thermometer_type_now">[+-][0-9][0-9]' index.html > temp.txt
+	RESULT=$(grep -E -o '[+-][0-9][0-9]' temp.txt)
+	echo $RESULT
+	rm temp.txt
+	rm index.html
+	sleep $time
+done
